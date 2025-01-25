9ad3cd0096732d82bff3f329d1be6abdb4b33008
artek <art.ekimov@mail.ru>
2025-01-25 12:01:04 +0500

    Создал примерную структуру программы. Нужно еще добавлять классы взаимодействия с программой. Нужно добавлять классы исключения ошибок.

--- src/Book.java
+++ src/Book.java
@@ -1,15 +1,22 @@
 public class Book {
-
+    private int id;
     private String title;
     private String author;
     private String genre;
     private int year;
 
+
+
     public Book(String title, String author, String genre, int year) {
+        this.id = id;
         this.title = title;
         this.author = author;
         this.genre = genre;
         this.year = year;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getTitle() {
