080c5cbab267a4e1cd46530898b0d5571be98a73
artek <art.ekimov@mail.ru>
2025-01-25 12:01:49 +0500

    Добавил классы библиотеки и пользователя

---
+++ src/Library.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Book> books;
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+}
---
+++ src/User.java
@@ -0,0 +1,20 @@
+public class User {
+
+    private String surname;
+    private String name;
+
+    public User(String surname, String name) {
+        this.surname = surname;
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
